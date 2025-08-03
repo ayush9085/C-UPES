8f168d7f55d58d7edb58bcd25b12261655592cce
ayush9085 <ayush1983pandey@gmail.com>
2025-08-03 19:36:52 +0530

    add

--- hello.c
+++ hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    // lets create a addition program by taking input from user
+    int a, b, sum;
+    printf("Enter two integers: ");
+    scanf("%d %d", &a, &b);
+    sum = a + b;
+    printf("Sum: %d\n", sum);
+
+    
     return 0;
 }
