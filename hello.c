7a5603406d9e53bf4dd1b633614c7e1da6a6d3cd
ayush9085 <ayush1983pandey@gmail.com>
2025-08-03 19:42:52 +0530

    add

--- hello.c
+++ hello.c
@@ -3,8 +3,10 @@
 int main() {
     // lets create a addition program by taking input from user
     int a, b, sum;
-    printf("Enter two integers: ");
-    scanf("%d %d", &a, &b);
+    printf("Enter First integer: ");
+    scanf("%d", &a);
+    printf("Enter second integer: ");
+    scanf("%d", &b);
     sum = a + b;
     printf("Sum: %d\n", sum);
 
