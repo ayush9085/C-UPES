ffdd1e5b2eaa1090ba9f83e7567c09001acbb568
ayush9085 <ayush1983pandey@gmail.com>
2025-08-03 20:14:46 +0530

    new

--- hello.c
+++ hello.c
@@ -8,7 +8,13 @@ int main() {
     printf("Enter second integer: ");
     scanf("%d", &b);
     sum = a + b;
-    printf("Sum: %d\n", sum);
+     if (sum>0){
+        printf("sum is positive\n");
+    } else if (sum<0){
+        printf("sum is negative\n");
+    } else {
+        printf("sum is zero\n");    
+     }
 
     
     return 0;
