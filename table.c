c290864706072bda61bfb618342b2708ef5c8b50
ayush9085 <ayush1983pandey@gmail.com>
2025-08-13 18:33:24 +0530

    table program

---
+++ table.c
@@ -0,0 +1,13 @@
+// A program which gives you the whole table of any number after you give the input.
+#include <stdio.h>
+
+int main() {
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    printf("Multiplication table of %d:\n", num);
+    for (int i = 1; i <= 10; i++) {
+        printf("%d x %d = %d\n", num, i, num * i);
+    }
+    return 0;
+}
