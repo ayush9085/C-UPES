a052a4ffe22ce5a2f1f050a5f9e160182522a8df
ayush9085 <ayush1983pandey@gmail.com>
2025-08-22 10:27:18 +0530

    solved a bug

--- Day2/Ques4.c
+++ Day2/Ques4.c
@@ -10,8 +10,8 @@ int main() {
     area = 3.14 * radius * radius;
     circumference = 2 * (3.14 * radius);
 
-    printf("\nArea of the rectangle: %.2f\n", area);
-    printf("Perimeter of the rectangle: %.2f\n", circumference);
+    printf("\nArea of the circle: %.2f\n", area);
+    printf("Perimeter of the circle %.2f\n", circumference);
 
     
     return 0;
