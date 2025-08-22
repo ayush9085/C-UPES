a505ee15c5a5cf9cb15255de5590b11738e8512d
ayush9085 <ayush1983pandey@gmail.com>
2025-08-22 10:22:20 +0530

    calculated the  area and circumference of a circle

---
+++ Day2/Ques4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    float radius, area, circumference;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%f", &radius);
+
+    area = 3.14 * radius * radius;
+    circumference = 2 * (3.14 * radius);
+
+    printf("\nArea of the rectangle: %.2f\n", area);
+    printf("Perimeter of the rectangle: %.2f\n", circumference);
+
+    
+    return 0;
+}
