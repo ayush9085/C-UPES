bbd7a299fd1cacb8ca61788fa06455b3df7c9f02
ayush9085 <ayush1983pandey@gmail.com>
2025-08-23 08:41:38 +0530

    added questions

---
+++ DAy3/Ques6.c
@@ -0,0 +1,15 @@
+// Q5: Write a program to convert temperature from Celsius to Fahrenheit.
+
+// Sample Test Cases:
+// Input 1:
+// 0
+// Output 1:
+// Fahrenheit=32
+
+// Input 2:
+// 100
+// Output 2:
+// Fahrenheit=212
+
+
+
