d04a9a894f2275b8ea4ded9782abc0875e04483d
ayush9085 <ayush1983pandey@gmail.com>
2025-08-28 11:27:29 +0530

    solved

--- Day8/Ques15.c
+++ Day8/Ques15.c
@@ -19,33 +19,9 @@ Digit
 Input 4:
 #
 Output 4:
-Special character
+Special character */
 
-*/
-#include <stdio.h>/*Q15: Write a program to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
-
-Sample Test Cases:
-Input 1:
-A
-Output 1:
-Uppercase alphabet
-
-Input 2:
-a
-Output 2:
-Lowercase alphabet
-
-Input 3:
-3
-Output 3:
-Digit
-
-Input 4:
-#
-Output 4:
-Special character
-
-*/
+ 
 #include <stdio.h>
 int main(){
     char ch;
