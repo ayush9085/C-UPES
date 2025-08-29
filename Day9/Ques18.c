7de319845ff810cf4d9eb25773b1ca936084c9db
ayush9085 <ayush1983pandey@gmail.com>
2025-08-29 19:01:39 +0530

    solved

--- Day9/Ques18.c
+++ Day9/Ques18.c
@@ -21,3 +21,28 @@ Input 4:
 Output 4:
 Grade F
 */
+#include <stdio.h>
+int main(){
+    float percent;
+    printf("Enter Your Percentage :");
+    scanf("%f" ,&percent);
+    if(percent >=90){
+        printf("Grade A");
+    }
+    else if(percent >=80){
+        printf("Grade B");
+    }
+    else if(percent>=70){
+        printf("Grade C");
+    }
+    else if(percent>=60){
+        printf("Grade D");
+    }
+    else if(percent >=50){
+        printf("Grade E");
+    }
+    else{
+        printf("Grade F ");
+    }
+    return 0;
+}
