ef89776da9f062c463a17c0a6845ad4902b240b7
ayush9085 <ayush1983pandey@gmail.com>
2025-08-29 13:17:39 +0530

    added Question

---
+++ Day9/Ques18.c
@@ -0,0 +1,23 @@
+/*Q18: Write a program to assign grades based on a percentage input.
+
+Sample Test Cases:
+Input 1:
+95
+Output 1:
+Grade A
+
+Input 2:
+82
+Output 2:
+Grade B
+
+Input 3:
+68
+Output 3:
+Grade D
+
+Input 4:
+50
+Output 4:
+Grade F
+*/
