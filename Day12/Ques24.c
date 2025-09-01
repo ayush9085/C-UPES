051ac35c9cd9419aab17c459d525b69d714661a9
ayush9085 <ayush1983pandey@gmail.com>
2025-09-01 17:45:27 +0530

    added Question

---
+++ Day12/Ques24.c
@@ -0,0 +1,18 @@
+//Q24: Write a program to calculate an electricity bill based on units consumed.
+/*
+Sample Test Cases:
+Input 1:
+50
+Output 1:
+Bill: ₹250
+
+Input 2:
+150
+Output 2:
+Bill: ₹950
+
+Input 3:
+250
+Output 3:
+Bill: ₹2200
+*/
