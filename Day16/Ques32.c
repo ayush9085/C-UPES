c970a0a0c06fb293cf3f9b09d3a3272ea49adb76
ayush9085 <ayush1983pandey@gmail.com>
2025-09-05 20:08:10 +0530

    solved

--- Day16/Ques32.c
+++ Day16/Ques32.c
@@ -11,3 +11,26 @@ Input 2:
 Output 2:
 Not palindrome
 */
+#include <stdio.h>
+
+int main() {
+    int num, original, reversed = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num > 0) {
+        reversed = reversed * 10 + (num % 10);
+        num /= 10;
+    }
+
+    if (original == reversed)
+        printf("Palindrome\n");
+    else
+        printf("Not palindrome\n");
+
+    return 0;
+}
+
