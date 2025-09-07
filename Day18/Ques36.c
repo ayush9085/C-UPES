ccbc46f9f42b22ae3f0b868f961ad1b4260fce21
ayush9085 <ayush1983pandey@gmail.com>
2025-09-07 11:17:18 +0530

    solved

--- Day18/Ques36.c
+++ Day18/Ques36.c
@@ -13,20 +13,4 @@ Output 2:
 1 2 5 10
 
 */
-#include <stdio.h>
-
-int main() {
-    int n, i;
-    printf("Enter a number: ");
-    scanf("%d", &n);
-
-    printf("Factors of %d are: ", n);
-    for (i = 1; i <= n; i++) {
-        if (n % i == 0) {
-            printf("%d ", i);
-        }
-    }
-    printf("\n");
-
-    return 0;
-}
+ 
