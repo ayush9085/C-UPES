733d41acd583f630f298e2655411c1e9c7b24d2b
ayush9085 <ayush1983pandey@gmail.com>
2025-09-07 11:28:10 +0530

    solved

--- Day18/Ques36.c
+++ Day18/Ques36.c
@@ -1,1 +1,27 @@
- 
+ #include <stdio.h>
+
+int main() {
+    int a, b;
+
+    // Input
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    // Boundary check
+    if (a <= 0 || b <= 0) {
+        printf("Numbers must be positive!\n");
+        return 1;
+    }
+
+    // Euclidean Algorithm
+    while (a != b) {
+        if (a > b)
+            a = a - b;
+        else
+            b = b - a;
+    }
+
+    printf("HCF (GCD) = %d\n", a);
+
+    return 0;
+}
