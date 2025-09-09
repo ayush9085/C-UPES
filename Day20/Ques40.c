62ae9ad92b806da0d02ff402cab33b8503fea407
ayush9085 <ayush1983pandey@gmail.com>
2025-09-09 18:07:28 +0530

    added

--- Day20/Ques40.c
+++ Day20/Ques40.c
@@ -13,3 +13,25 @@ Output 2:
 0000
 
 */
+#include <stdio.h>
+
+int main() {
+    char binary[100]; // to store binary number as string
+    printf("Enter a binary number: ");
+    scanf("%s", binary);
+
+    // Traverse through each digit
+    for (int i = 0; binary[i] != '\0'; i++) {
+        if (binary[i] == '0')
+            binary[i] = '1';
+        else if (binary[i] == '1')
+            binary[i] = '0';
+        else {
+            printf("Invalid binary input!\n");
+            return 1;
+        }
+    }
+
+    printf("1's complement: %s\n", binary);
+    return 0;
+}
