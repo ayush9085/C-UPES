0c1c4571f2ac45e2241c9f8658f49cdd16428990
ayush9085 <ayush1983pandey@gmail.com>
2025-09-16 18:15:45 +0530

    added

---
+++ Day27/Ques54.c
@@ -0,0 +1,22 @@
+/*Q54: Write a program to print the following pattern:
+
+*
+
+***
+
+*****
+*******
+***
+
+*
+
+
+
+/*
+Sample Test Cases:
+Input 1:
+
+Output 1:
+Pattern with layers of stars as shown.
+
+*/
