ed14e697f47d9040bb485ee42a759342f47a96f5
ayush9085 <ayush1983pandey@gmail.com>
2025-09-20 07:50:12 +0530

    started

---
+++ Day31/Ques61.c
@@ -0,0 +1,23 @@
+/*Q61: Search for an element in an array using linear search.
+
+Sample Test Cases:
+Input 1:
+5
+1 2 3 4 5
+3
+Output 1:
+Found at index 2
+
+Input 2:
+4
+10 20 30 40
+25
+Output 2:
+-1
+
+*/
+#include <stdio.h>
+int main(){
+    int n,x,i;
+    printf;
+}
