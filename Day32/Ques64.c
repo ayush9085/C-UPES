cc29fa1496689e07db5e638cceb0264100e3badd
ayush9085 <ayush1983pandey@gmail.com>
2025-09-21 18:34:21 +0530

    solved

--- Day32/Ques64.c
+++ Day32/Ques64.c
@@ -14,16 +14,26 @@ Output 2:
 */
  #include <stdio.h>
 int main(){
-    int n;
+     long long int n;
     int freq[10]={0};
     printf("Enter the Number :");
-    scanf("%d" ,&n);
+    scanf("%lld" ,&n);
     while(n>0){
         int digit = n%10;
         freq[digit]++;
         n/=10;
     }
-    
+    int max_freq=0, ans=0;
+    for(int i=0;i<10;i++){
+        if(freq[i]>max_freq){
+            max_freq=freq[i];
+            ans=i;
+        }
+    }
+    printf("THe number whis appeared maximum times is :%d" , ans);
+    return 0;
+
+
     
 
 }
