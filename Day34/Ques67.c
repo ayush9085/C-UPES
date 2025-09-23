788bc1df69b1f4728d4c085d13be2ecf55baf952
ayush9085 <ayush1983pandey@gmail.com>
2025-09-23 08:04:20 +0530

    Added

--- Day34/Ques67.c
+++ Day34/Ques67.c
@@ -9,31 +9,37 @@ Output 1:
 10 20 15 30 40
 
 */
- #include <stdio.h>
+  #include <stdio.h>
+
 int main() {
-    int arr[20] = {1, 2, 3, 4, 5};
-    int n = 5;  // current size
-    int pos, val;
+    int arr[50], n, pos, val;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
 
     printf("Enter position to insert (1-%d): ", n+1);
     scanf("%d", &pos);
     printf("Enter value: ");
     scanf("%d", &val);
 
-    if(pos < 1 || pos > n+1){
+    if(pos < 1 || pos > n+1) {
         printf("Invalid position!\n");
     } else {
-        // shift elements to right
-        for(int i = n; i >= pos; i--){
+        for(int i = n; i >= pos; i--) {
             arr[i] = arr[i-1];
         }
         arr[pos-1] = val;
-        n++;  // size increases
+        n++;
+    }
 
-        printf("Array after insertion: ");
-        for(int i = 0; i < n; i++){
-            printf("%d ", arr[i]);
-        }
+    printf("Array after insertion: ");
+    for(int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
     }
     return 0;
 }
