96f337ec02870666fe01c03b5ada7bcb4a4691ef
ayush9085 <ayush1983pandey@gmail.com>
2025-09-26 09:36:07 +0530

    solving

--- Day36/Ques72.c
+++ Day36/Ques72.c
@@ -9,3 +9,26 @@ Output 1:
 21
 
 */
+#include <stdio.h>
+int main(){
+    int row,column;
+    printf("Enter no. Row");
+    scanf("%d"  , &row);
+     printf("Enter no. column");
+    scanf("%d"  , &column);
+    int arr[row][column];
+    //lets take input
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            scanf("%d" ,&arr[i][j]);
+        }
+    }
+    //now printing the array
+    for(int i=0;i<row;i++){
+        for(int j=0;j<column;j++){
+            printf("%d " ,arr[i][j]);
+        }
+        printf("\n"); 
+    }
+return 0;
+}
