42db892d66a75f24c209967667d59b0281f43a74
ayush9085 <ayush1983pandey@gmail.com>
2025-09-27 18:37:06 +0530

    solving

--- Day38/Ques76.c
+++ Day38/Ques76.c
@@ -16,3 +16,37 @@ Output 2:
 False
 
 */
+#include <stdio.h>
+
+void readMatrix(int r, int c, int m[r][c]) {
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            scanf("%d", &m[i][j]);
+        }
+    }
+}
+
+void printMatrix(int r, int c, int m[r][c]) {
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            printf("%d ", m[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void addMatrices(int r, int c, int a[r][c], int b[r][c], int sum[r][c]) {
+    for(int i = 0; i < r; i++) {
+        for(int j = 0; j < c; j++) {
+            sum[i][j] = a[i][j] + b[i][j];
+        }
+    }
+}
+
+int main() {
+    int r1, c1, r2, c2;
+    scanf("%d %d", &r1, &c1);
+    int a[r1][c1];
+    readMatrix(r1, c1, a);
+
+     
