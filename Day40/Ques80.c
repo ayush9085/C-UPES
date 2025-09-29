328183baf3bd5d145ab28b52eebae82a06be8523
ayush9085 <ayush1983pandey@gmail.com>
2025-09-29 09:38:14 +0530

    solving

--- Day40/Ques80.c
+++ Day40/Ques80.c
@@ -1,11 +1,17 @@
-/*Q79: Perform diagonal traversal of a matrix.
+ Q80: Multiply two matrices.
+
+/*
 Sample Test Cases:
 Input 1:
-3 3
+2 3
 1 2 3
 4 5 6
-7 8 9
+3 2
+7 8
+9 10
+11 12
 Output 1:
-1 2 4 7 5 3 6 8 9
+58 64
+139 154
 
 */
