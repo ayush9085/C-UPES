8b9eb27fb8e5402f2c97ee88b6733cbf2d3f6e18
ayush9085 <ayush1983pandey@gmail.com>
2025-10-04 15:57:55 +0530

    soving

--- Day45/Ques89.c
+++ Day45/Ques89.c
@@ -8,3 +8,29 @@ Output 1:
 2
 
 */
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+    char ch;
+    int count = 0;
+
+    // Input string
+    scanf("%s", str);
+
+    // Input character
+    scanf(" %c", &ch); // Notice the space before %c to skip newline/whitespace
+
+    // Count frequency
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ch) {
+            count++;
+        }
+    }
+
+    // Output
+    printf("%d\n", count);
+
+    return 0;
+}
