aa3ebde6ea4ee70c98294e61a5cb43cafd7b2c11
ayush9085 <ayush1983pandey@gmail.com>
2025-10-04 15:59:21 +0530

    soving

--- Day45/Ques90.c
+++ Day45/Ques90.c
@@ -7,3 +7,20 @@ Output 1:
 hELLO
 
 */
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    scanf("%s", str);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] >= 'a' && str[i] <= 'z') {
+            str[i] = str[i] - 32; // to uppercase
+        } else if (str[i] >= 'A' && str[i] <= 'Z') {
+            str[i] = str[i] + 32; // to lowercase
+        }
+    }
+
+    printf("%s\n", str);
+    return 0;
+}
