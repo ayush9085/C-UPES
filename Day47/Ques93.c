3a73a7a8efefe2aa6a399485e499d42c43c37c29
ayush9085 <ayush1983pandey@gmail.com>
2025-10-06 18:20:54 +0530

    solving

---
+++ Day47/Ques93.c
@@ -0,0 +1,17 @@
+/*Q93: Check if two strings are anagrams of each other.
+
+/*
+Sample Test Cases:
+Input 1:
+listen
+silent
+Output 1:
+Anagrams
+
+Input 2:
+hello
+world
+Output 2:
+Not anagrams
+
+*/
