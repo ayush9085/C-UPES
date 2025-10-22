49ceb024e75fa01ae652c1417faae8af778727ef
ayush9085 <ayush1983pandey@gmail.com>
2025-10-22 18:10:13 +0530

    solving

--- Day62/Ques112.c
+++ Day62/Ques112.c
@@ -1,6 +1,5 @@
-Q112: Write a program to take an integer array arr as input. The task is to find the maximum sum of any contiguous subarray using Kadane's algorithm. Print the maximum sum as output. If all elements are negative, print the largest (least negative) element.
+/*Q112: Write a program to take an integer array arr as input. The task is to find the maximum sum of any contiguous subarray using Kadane's algorithm. Print the maximum sum as output. If all elements are negative, print the largest (least negative) element.
 
-/*
 Sample Test Cases:
 Input 1:
 arr[] = [2, 3, -8, 7, -1, 2, 3]
