6fb788640202fac5058b098fa55ef8c3dadd876f
ayush9085 <ayush1983pandey@gmail.com>
2025-10-22 17:56:51 +0530

    solving

---
+++ Day63/Ques113.c
@@ -0,0 +1,14 @@
+/*Q113: Write a program to take an integer array arr and an integer k as inputs. The task is to find the kth smallest element in the array. Print the kth smallest element as output.
+
+Sample Test Cases:
+Input 1:
+arr[] = [10, 5, 4, 3, 48, 6, 2, 33, 53, 10], k = 4
+Output 1:
+5
+
+Input 2:
+arr[] = [7, 10, 4, 3, 20, 15], k = 3
+Output 2:
+7
+
+*/
