8473f2abfc82cbd103f23892f750044f0cec9ef5
ayush9085 <ayush1983pandey@gmail.com>
2025-10-23 08:02:54 +0530

    solved

--- Day64/Ques114.c
+++ Day64/Ques114.c
@@ -17,3 +17,29 @@ Output 3:
 3
 
 */
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char s[100];
+    int i, j, maxLen = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", s);
+
+    int n = strlen(s);
+
+    for (i = 0; i < n; i++) {
+        int visited[256] = {0}; // Track characters in current substring
+        for (j = i; j < n; j++) {
+            if (visited[(int)s[j]] == 1)  // If repeated
+                break;
+            visited[(int)s[j]] = 1;
+            if (j - i + 1 > maxLen)
+                maxLen = j - i + 1;
+        }
+    }
+
+    printf("%d", maxLen);
+    return 0;
+}
