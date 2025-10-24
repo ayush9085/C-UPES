0065f108d0639b29a99f5574952dd1fa169ba5d2
ayush9085 <ayush1983pandey@gmail.com>
2025-10-24 07:29:29 +0530

    fix comment formatting in anagram validation program

--- Day65/Ques115.c
+++ Day65/Ques115.c
@@ -1,6 +1,5 @@
-Q115: Write a program to take two strings s and t as inputs (assume all characters are lowercase). The task is to determine if s and t are valid anagrams, meaning they contain the same characters with the same frequencies. Print "Anagram" if they are, otherwise "Not Anagram".
+/*Q115: Write a program to take two strings s and t as inputs (assume all characters are lowercase). The task is to determine if s and t are valid anagrams, meaning they contain the same characters with the same frequencies. Print "Anagram" if they are, otherwise "Not Anagram".
 
-/*
 Sample Test Cases:
 Input 1:
 s = "anagram", t = "nagaram"
