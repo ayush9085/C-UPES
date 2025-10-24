9e868bab36ea38920a93532428331d3fb775c1b3
ayush9085 <ayush1983pandey@gmail.com>
2025-10-24 07:29:09 +0530

    add initial implementation for anagram validation program

---
+++ Day65/Ques115.c
@@ -0,0 +1,15 @@
+Q115: Write a program to take two strings s and t as inputs (assume all characters are lowercase). The task is to determine if s and t are valid anagrams, meaning they contain the same characters with the same frequencies. Print "Anagram" if they are, otherwise "Not Anagram".
+
+/*
+Sample Test Cases:
+Input 1:
+s = "anagram", t = "nagaram"
+Output 1:
+Anagram
+
+Input 2:
+s = "rat", t = "car"
+Output 2:
+Not Anagram
+
+*/
