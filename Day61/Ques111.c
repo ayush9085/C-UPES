e7f2f668c407652f51b3a01b27d9f59c5ced197a
ayush9085 <ayush1983pandey@gmail.com>
2025-10-24 17:30:24 +0530

    solved

--- Day61/Ques111.c
+++ Day61/Ques111.c
@@ -17,3 +17,25 @@ Output 3:
 0 0
 
 */
+#include <stdio.h>
+
+int main() {
+    int arr[] = {-8, 2, 3, -6, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 2;
+
+    for (int i = 0; i <= n - k; i++) {
+        int found = 0; // flag to check if negative found
+        for (int j = i; j < i + k; j++) {
+            if (arr[j] < 0) {
+                printf("%d ", arr[j]);
+                found = 1;
+                break; // first negative mil gaya, window ka kaam khatam
+            }
+        }
+        if (!found)
+            printf("0 ");
+    }
+
+    return 0;
+}
