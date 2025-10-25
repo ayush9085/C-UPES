411b56a0e65bce51e9fdb81725d438cf7cb77c56
ayush9085 <ayush1983pandey@gmail.com>
2025-10-25 07:55:32 +0530

    solved

--- Day60/Ques110.c
+++ Day60/Ques110.c
@@ -12,3 +12,25 @@ Output 2:
 5 1 3 4 2
 
 */
+#include <stdio.h>
+
+// Function to find the maximum in each subarray of size k
+void printMaxInSubarrays(int arr[], int n, int k) {
+    for (int i = 0; i <= n - k; i++) {
+        int max = arr[i];
+        for (int j = 1; j < k; j++) {
+            if (arr[i + j] > max)
+                max = arr[i + j];
+        }
+        printf("%d ", max);
+    }
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 1, 4, 5, 2, 3, 6};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int k = 3;
+
+    printMaxInSubarrays(arr, n, k);
+    return 0;
+}
