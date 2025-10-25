b101840817e23a3acc41af087f066745fe3c1c4e
ayush9085 <ayush1983pandey@gmail.com>
2025-10-25 07:37:53 +0530

    commented out the Question

--- Day66/Ques116.c
+++ Day66/Ques116.c
@@ -1,6 +1,5 @@
-Q116: Write a program to take an integer array nums which contains only positive integers, and an integer target as inputs. The goal is to find two distinct indices i and j in the array such that nums[i] + nums[j] equals the target. Assume exactly one solution exists and return the indices in any order. Print the two indices separated by a space as output. If no solution exists, print "-1 -1".
+/*Q116: Write a program to take an integer array nums which contains only positive integers, and an integer target as inputs. The goal is to find two distinct indices i and j in the array such that nums[i] + nums[j] equals the target. Assume exactly one solution exists and return the indices in any order. Print the two indices separated by a space as output. If no solution exists, print "-1 -1".
 
-/*
 Sample Test Cases:
 Input 1:
 nums = [2,7,11,15], target = 9
