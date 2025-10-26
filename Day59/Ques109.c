35f4db2966e423d1565a1c2ee132db2f51286f33
ayush9085 <ayush1983pandey@gmail.com>
2025-10-26 07:39:49 +0530

    solved

--- Day59/Ques109.c
+++ Day59/Ques109.c
@@ -17,3 +17,42 @@ Output 3:
 400
 
 */
+#include <stdio.h>
+
+int main() {
+    int n, k;
+    printf("Enter the size of array: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter the elements of array: ");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter k: ");
+    scanf("%d", &k);
+
+    if (k > n || k <= 0) {
+        printf("Invalid window size\n");
+        return 0;
+    }
+
+    int windowSum = 0;
+    for (int i = 0; i < k; i++) {
+        windowSum += arr[i];
+    }
+
+    int maxSum = windowSum;
+
+    for (int i = k; i < n; i++) {
+        windowSum += arr[i] - arr[i - k];
+        if (windowSum > maxSum) {
+            maxSum = windowSum;
+        }
+    }
+
+    printf("%d\n", maxSum);
+
+    return 0;
+}
