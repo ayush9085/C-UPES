a215d2097403e116cd9e8408542799a86b48fdf4
ayush9085 <ayush1983pandey@gmail.com>
2025-11-01 08:01:28 +0530

    solving\

--- Day69/Ques119.c
+++ Day69/Ques119.c
@@ -1,6 +1,5 @@
 /*Q119: Write a program to take an integer array as input. Only one element will be repeated. Print the repeated element. Try to find the result in one single iteration.
 
-/*
 Sample Test Cases:
 Input 1:
 nums1 = [1,3,3,4]
