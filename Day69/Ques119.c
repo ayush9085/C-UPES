64a3d0787c242087e8ee58584f4cdfe0b696e82c
ayush9085 <ayush1983pandey@gmail.com>
2025-11-01 08:01:52 +0530

    solved

--- Day69/Ques119.c
+++ Day69/Ques119.c
@@ -17,3 +17,28 @@ Output 3:
 1
 
 */
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    int visited[1000] = {0}; // assuming elements are <1000 for simplicity
+
+    printf("Enter %d elements:\n", n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (visited[arr[i]] == 1) { // repeated found
+            printf("Repeated element: %d\n", arr[i]);
+            break;
+        }
+        visited[arr[i]] = 1;
+    }
+
+    return 0;
+}
