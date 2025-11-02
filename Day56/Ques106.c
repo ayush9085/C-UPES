d25af05ef47cddd68d83b539abffd77d246846f1
ayush9085 <ayush1983pandey@gmail.com>
2025-11-02 15:54:32 +0530

    solved

--- Day56/Ques106.c
+++ Day56/Ques106.c
@@ -26,3 +26,30 @@ Output 4:
 -1, -1, -1, -1
 
 */
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for(int i = 0; i < n; i++) {
+        int nextGreater = -1;
+        for(int j = i + 1; j < n; j++) {
+            if(arr[j] > arr[i]) {
+                nextGreater = arr[j];
+                break;
+            }
+        }
+        printf("%d", nextGreater);
+        if(i != n - 1)
+            printf(", ");
+    }
+
+    return 0;
+}
