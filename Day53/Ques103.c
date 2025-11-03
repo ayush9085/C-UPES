9e4a9402a90353cc4ec849e12aafb2de932b19a0
ayush9085 <ayush1983pandey@gmail.com>
2025-11-03 09:45:09 +0530

    solved

--- Day53/Ques103.c
+++ Day53/Ques103.c
@@ -17,3 +17,33 @@ Output 3:
 0
 
 */
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter size of array: ");
+    scanf("%d", &n);
+
+    int nums[n];
+    printf("Enter %d elements: ", n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &nums[i]);
+    }
+
+    int totalSum = 0, leftSum = 0;
+    for(int i = 0; i < n; i++) {
+        totalSum += nums[i];
+    }
+
+    for(int i = 0; i < n; i++) {
+        int rightSum = totalSum - leftSum - nums[i];
+        if(leftSum == rightSum) {
+            printf("%d\n", i);
+            return 0;
+        }
+        leftSum += nums[i];
+    }
+
+    printf("-1\n");
+    return 0;
+}
