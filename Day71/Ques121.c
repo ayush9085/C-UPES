d852cd8be1891e03d93005f05193a2f44f56abf4
ayush9085 <ayush1983pandey@gmail.com>
2025-11-03 09:43:00 +0530

    solved

--- Day71/Ques121.c
+++ Day71/Ques121.c
@@ -7,3 +7,36 @@ Output 1:
 File created successfully! Data written to info.txt
 
 */
+#include <stdio.h>
+
+int main() {
+    FILE *fp;
+    char name[50];
+    int age;
+
+    // Open file in write mode
+    fp = fopen("info.txt", "w");
+
+    if (fp == NULL) {
+        printf("Error! Could not create file.\n");
+        return 1;
+    }
+
+    // Take user input
+    printf("Enter your name: ");
+    scanf("%s", name);
+
+    printf("Enter your age: ");
+    scanf("%d", &age);
+
+    // Write data to file
+    fprintf(fp, "Name: %s\nAge: %d\n", name, age);
+
+    // Close the file
+    fclose(fp);
+
+    // Display confirmation message
+    printf("File created successfully! Data written to info.txt\n");
+
+    return 0;
+}
