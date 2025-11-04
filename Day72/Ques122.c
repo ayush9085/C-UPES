b1a676d5cff2cc5198b5a668b8f54cd2ec3d960c
ayush9085 <ayush1983pandey@gmail.com>
2025-11-04 17:00:07 +0530

    solving

--- Day72/Ques122.c
+++ Day72/Ques122.c
@@ -1,6 +1,5 @@
 /*Q122: Write a C program that opens an existing file (e.g., info.txt) and reads its contents using fgets(). The program should print all the lines to the console until EOF (end of file) is reached.
 
-/*
 Sample Test Cases:
 Input 1:
 File: info.txt (Content: Name: Rahul\nAge: 23)
