fb96f9e0dfc1b1c606368356e79c228416b6d28d
ayush9085 <ayush1983pandey@gmail.com>
2025-11-05 07:50:18 +0530

    solved

--- Day73/Ques123.c
+++ Day73/Ques123.c
@@ -9,3 +9,49 @@ Words: 5
 Lines: 2
 
 */
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+    FILE *file;
+    char filename[100], ch;
+    int characters = 0, words = 0, lines = 0;
+    int inWord = 0;
+
+    printf("Enter filename: ");
+    scanf("%s", filename);
+
+    file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("Error: Could not open file %s\n", filename);
+        return 1;
+    }
+
+    while ((ch = fgetc(file)) != EOF) {
+        characters++;
+
+        // Count lines
+        if (ch == '\n')
+            lines++;
+
+        // Count words
+        if (isspace(ch))
+            inWord = 0;
+        else if (inWord == 0) {
+            inWord = 1;
+            words++;
+        }
+    }
+
+    fclose(file);
+
+    // If file isn't empty but has no '\n', we still have 1 line
+    if (characters > 0 && lines == 0)
+        lines = 1;
+
+    printf("Characters: %d\n", characters);
+    printf("Words: %d\n", words);
+    printf("Lines: %d\n", lines);
+
+    return 0;
+}
