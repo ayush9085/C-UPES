da761306b46efb024194c71517b9eb6398dbb88e
ayush9085 <ayush1983pandey@gmail.com>
2025-11-06 22:08:00 +0530

    solving

---
+++ Day74/Ques124.c
@@ -0,0 +1,10 @@
+Q124: Take two filenames from the user – a source file and a destination file. Copy all the content from the source file to the destination file using fgetc() and fputc().
+
+/*
+Sample Test Cases:
+Input 1:
+Source File: source.txt (Content: Learning C File Handling)
+Output 1:
+File copied successfully to destination.txt
+
+*/
