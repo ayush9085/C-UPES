e727fc974d421e5fa730171356a1890b2764196e
ayush9085 <ayush1983pandey@gmail.com>
2025-11-06 22:08:47 +0530

    solved

--- Day74/Ques124.c
+++ Day74/Ques124.c
@@ -7,3 +7,46 @@ Output 1:
 File copied successfully to destination.txt
 
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *src, *dest;
+    char srcFile[100], destFile[100];
+    char ch;
+
+    // Take filenames from user
+    printf("Enter source filename: ");
+    scanf("%s", srcFile);
+
+    printf("Enter destination filename: ");
+    scanf("%s", destFile);
+
+    // Open source file for reading
+    src = fopen(srcFile, "r");
+    if (src == NULL) {
+        printf("Error: Cannot open source file.\n");
+        return 1;
+    }
+
+    // Open destination file for writing
+    dest = fopen(destFile, "w");
+    if (dest == NULL) {
+        printf("Error: Cannot open destination file.\n");
+        fclose(src);
+        return 1;
+    }
+
+    // Copy contents using fgetc() and fputc()
+    while ((ch = fgetc(src)) != EOF) {
+        fputc(ch, dest);
+    }
+
+    printf("File copied successfully to %s\n", destFile);
+
+    // Close both files
+    fclose(src);
+    fclose(dest);
+
+    return 0;
+}
