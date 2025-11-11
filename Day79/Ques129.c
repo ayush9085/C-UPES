6827f1721e34b2b4949c886ccb87f5f371006cc4
ayush9085 <ayush1983pandey@gmail.com>
2025-11-11 18:32:11 +0530

    solving

--- Day79/Ques129.c
+++ Day79/Ques129.c
@@ -1,6 +1,5 @@
 /*Q129: A file numbers.txt contains a list of integers separated by spaces. Read all integers, compute their sum and average, and print both.
 
-/*
 Sample Test Cases:
 Input 1:
 File: numbers.txt (Content: 10 20 30 40 50)
