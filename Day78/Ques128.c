bce0f2f7c0d78c0386e5d9ce34b68507fb091b39
ayush9085 <ayush1983pandey@gmail.com>
2025-11-12 07:39:52 +0530

    solving

--- Day78/Ques128.c
+++ Day78/Ques128.c
@@ -8,3 +8,39 @@ Vowels: 8
 Consonants: 10
 
 */
+#include <stdio.h>
+#include <ctype.h>  // for isalpha() and tolower()
+
+int main() {
+    FILE *fp;
+    char filename[100], ch;
+    int vowels = 0, consonants = 0;
+
+    printf("Enter filename: ");
+    scanf("%s", filename);
+
+    fp = fopen(filename, "r");
+
+    if (fp == NULL) {
+        printf("Error opening file.\n");
+        return 1;
+    }
+
+    while ((ch = fgetc(fp)) != EOF) {
+        ch = tolower(ch);  // convert to lowercase for easy checking
+
+        if (isalpha(ch)) {  // check if it's a letter
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+                vowels++;
+            else
+                consonants++;
+        }
+    }
+
+    fclose(fp);
+
+    printf("Vowels: %d\n", vowels);
+    printf("Consonants: %d\n", consonants);
+
+    return 0;
+}
