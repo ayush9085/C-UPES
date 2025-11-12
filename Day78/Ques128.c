d0ef7157045e1bcfc2aef2c2609d2bf657888f93
ayush9085 <ayush1983pandey@gmail.com>
2025-11-12 07:39:08 +0530

    Fix comment formatting for clarity in vowel and consonant counting program

--- Day78/Ques128.c
+++ Day78/Ques128.c
@@ -1,4 +1,4 @@
-Q128: Read a text file and count how many vowels and consonants are in the file. Ignore digits and special characters.
+/*Q128: Read a text file and count how many vowels and consonants are in the file. Ignore digits and special characters.
 
 /*
 Sample Test Cases:
