0f8c7ee1ae39e6d1575511043d51773cc24bcb07
ayush9085 <ayush1983pandey@gmail.com>
2025-11-13 07:43:40 +0530

    add problem statement and sample test cases for vowel and consonant counting

---
+++ Day77/Ques127.c
@@ -0,0 +1,11 @@
+Q128: Read a text file and count how many vowels and consonants are in the file. Ignore digits and special characters.
+
+/*
+Sample Test Cases:
+Input 1:
+File: text.txt (Content: Welcome to C language)
+Output 1:
+Vowels: 8
+Consonants: 10
+
+*/
